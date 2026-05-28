b78064730a31a5ecfd1cd12783b7bc2f093548b3
Chander Aima <chander.aima@gmail.com>
2026-05-28 10:46:15 +0530

    updated 6

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/home-printer-docs/',
-  trailingSlash: false,
+  trailingSlash: true,
   // GitHub pages deployment config.
   organizationName: 'techcom-writer',
   projectName: 'home-printer-docs',
